536287936f120c4367ba1d260cf24517a14c3462
Rohan Yadav <rajrohanyadav@gmail.com>
2021-08-28 16:23:20 +0530

    update href for anchor in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a href="#" className="title-font font-medium text-white mb-4 md:mb-0">
+        <a href="/" className="title-font font-medium text-white mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
             Rohan Yadav
           </a>
